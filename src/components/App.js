fdc578a46316799d2884bad3a331c895a5afe75a
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-06 01:32:29 +0500

    Add route component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Accordion from './Accordion';
 import Search from './Search';
 import Dropdown from './Dropdown';
 import Translate from './Translate';
+import Route from './Route';
 
 const items = [
   {
@@ -35,9 +36,26 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Route path='/' >
+        <Accordion items={items} />
+      </Route>
+      <Route path='/search' >
+        <Search />
+      </Route>
+      <Route path='/dropdown' >
+        <Dropdown
+          label='Select a color'
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path='/translate' >
+        <Translate />
+      </Route>
     </div>
   );
 }
--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,33 +5,31 @@ const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState('');
   const [debouncedText, setDebouncedText] = useState(text);
 
-  useEffect(()=>{
-    const timerId = setTimeout(()=>{
+  useEffect(() => {
+    const timerId = setTimeout(() => {
       setDebouncedText(text);
-    }, 500);
+    }, 1000);
 
-    return(()=>{
+    return (() => {
       clearTimeout(timerId);
     });
-  },[text]);
+  }, [text]);
 
-  useEffect(()=>{
-    const doTranslation = async () =>{
+  useEffect(() => {
+    const doTranslation = async () => {
       const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
         params: {
-          q: text,
+          q: debouncedText,
           target: language.value,
           key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
         }
       });
       setTranslated(data.data.translations[0].translatedText);
     }
-
     doTranslation();
-    
-  },[language, text]);
+  }, [language, debouncedText]);
 
-  
+
   return (
     <div>
       <h1 className="ui header" > {translated} </h1>
